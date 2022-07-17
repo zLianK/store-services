eddc18758220708e1f43a10de13a737d02dd1707
Lían König <lk.liankonig@gmail.com>
2022-07-16 21:18:06 -0300

    adding the customer module

---
+++ customer/src/test/java/com/lk/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
